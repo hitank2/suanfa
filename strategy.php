591a3496ecaacb77ebae3adbeb0a3360fb7e7166
hitank <644697546@qq.com>
2018-03-27 23:11:53 +0800

    asa

---
+++ strategy.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * 策略模式
+ * User: hitank
+ * Date: 2018/3/27
+ * Time: 22:57
+ */
+//抽象策略接口
+abstract class Strategy{
+    abstract function wayToSchool();
+}
+//具体策略角色
+class BikeStrategy extends Strategy{
+    function wayToSchool(){
+        echo "骑自行车去上学";
+    }
+}
+class BusStrategy extends Strategy{
+    function wayToSchool(){
+        echo "乘公共汽车去上学";
+    }
+}
+class TaxiStrategy extends Strategy{
+    function wayToSchool(){
+        echo "出租车去上学";
+    }
+}
+
+//环境角色
+class Context{
+    private $strategy;
+    //获取具体策略
+    function getStrategy($strategyName){
+        try{
+            $strategyReflection = new ReflectionClass($strategyName);
+            $this->strategy = $strategyReflection->newInstance();
+
+        }catch(ReflectionException $e){
+            $this->strategy = "";
+        }
+    }
+
+    function goToSchool(){
+        $this->strategy->wayToSchool();
+        // var_dump($this->strategy);
+    }
+}
+
+//测试
+$context = new Context();
+$context->getStrategy("BusStrategy");
+$context->goToSchool();
+ ?>
